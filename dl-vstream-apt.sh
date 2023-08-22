3d7ce21bbeed9542d507265fb315b5ed58d8fc83
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-08-22 13:32:51 +0800

    uploaded `.sh` executables for *nix distributions

---
+++ dl-vstream-apt.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+read -p "enter a VStream URL here: " url
+read -p "enter a file name to save to: " file
+
+echo
+echo "updating yt-dlp and downloading file. standby..."
+sudo apt-get update
+sudo apt-get -y install yt-dlp
+sudo apt-get -y autoremove
+sudo apt-get clean
+sudo apt-get -y autoclean
+yt-dlp -o vstream_$file.mkv --downloader ffmpeg $url --verbose
+
+echo
+echo "converting file to MP4. standby..."
+ffmpeg -loglevel debug -i vstream_$file.mkv -codec copy vstream_$file.mp4
+
+echo
+echo "deleting MKV file. standby..."
+rm -rf vstream_$file.mkv
+
+echo
+echo "download completed and converted successfully."
---
+++ dl-vstream-selfupdate.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+read -p "enter a VStream URL here: " url
+read -p "enter a file name to save to: " file
+
+echo
+echo "updating yt-dlp and downloading file. standby..."
+yt-dlp -U
+yt-dlp -o vstream_$file.mkv --downloader ffmpeg $url --verbose
+
+echo
+echo "converting file to MP4. standby..."
+ffmpeg -loglevel debug -i vstream_$file.mkv -codec copy vstream_$file.mp4
+
+echo
+echo "deleting MKV file. standby..."
+rm -rf vstream_$file.mkv
+
+echo
+echo "download completed and converted successfully."
